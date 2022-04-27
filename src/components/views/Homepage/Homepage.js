d61b15953c4acc3d79cc77d79a329053e590d994
Anna Kuszal <anna.kuszal@o2.pl>
2022-04-27 19:15:26 +0200

    Add Homepage (component& styles)

---
+++ src/components/views/Homepage/Homepage.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import styles from './Homepage.module.scss';
+
+import Skills from '../../layout/Skills/Skills';
+import Experience from '../../layout/Experience/Experience';
+import Courses from '../../layout/Courses/Courses';
+import Inspirations from '../../layout/Inspirations/Inspirations';
+import LatestFirstChoice from '../../layout/LatestFirstChoice/LatestFirstChoice';
+
+
+const Homepage = () => (
+  <div className={styles.root}>
+    <Skills title='Skills'/>
+    <Experience title='Experience'/>
+    <Courses title='Courses'/>
+    <Inspirations title='Inspirations'/>
+    <LatestFirstChoice title='My latest first choice'/>
+  </div>
+);
+
+
+export default Homepage;
